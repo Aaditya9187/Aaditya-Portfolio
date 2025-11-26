4c130f224c41d9ee85b7c18e4525b8ef9aba4f8c
Swastik <sharmaspeedx29@gmail.com>
2025-11-26 15:32:17 +0530

    Navbar

---
+++ src/components/NavBar.jsx
@@ -0,0 +1,41 @@
+import dayjs from 'dayjs'
+import { navIcons, navLinks } from "#constants";
+import useWindowStore from '#store/window';
+
+const NavBar = () => {
+
+  const {openWindow}= useWindowStore();
+  
+  return (
+    <nav>
+      <div>
+        <img src="/images/logo.svg" alt="logo" />
+        <p className="font-bold">Swastik's Portfolio</p>
+        <ul>
+          {navLinks.map(({ name, id, type }) => (
+            <li key={id} onClick={() => openWindow(type)}>
+              <p>{name}</p>
+            </li>
+          ))}
+        </ul>
+      </div>
+      <div>
+        <ul>
+          {navIcons.map(({ id, img }) => (
+            <li key={id}>
+              <img 
+                src={img} className="icon-hover"
+                alt={`icon-${id}`}
+              />
+            </li>
+          ))}
+        </ul>
+        <time>
+          {dayjs().format('ddd MMM D h:mm A')}
+        </time>
+      </div>
+    </nav>
+  );
+};
+
+export default NavBar;
