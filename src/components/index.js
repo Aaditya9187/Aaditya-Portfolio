4f3e9ffaded63861712bba80e6583604ecdd932b
Swastik <sharmaspeedx29@gmail.com>
2025-11-26 15:32:00 +0530

    Docks

---
+++ src/components/index.js
@@ -0,0 +1,6 @@
+import NavBar from "./NavBar";
+import Welcome from "#components/Welcome";
+import Dock from "./Dock";
+import WindowControls from "./WindowControls";
+
+export { NavBar, Welcome, Dock, WindowControls}
