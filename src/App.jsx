1e8c85e92f8b5370c41cfceeb467086163b1d7c0
Swastik <sharmaspeedx29@gmail.com>
2025-11-29 17:01:08 +0530

    home section

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,21 @@
 import gsap from 'gsap'
 import { Draggable } from 'gsap/Draggable'
+import { useEffect } from 'react'
 
-import { NavBar, Welcome, Dock } from '#components';
-import { Finder, Resume, Safari, Terminal, Text, Image, Contact } from '#windows';
+import { NavBar, Welcome, Dock, Home } from '#components';
+import { Finder, Resume, Safari, Terminal, Text, Image, Contact, Photos } from '#windows';
 
 gsap.registerPlugin(Draggable);
 
 const App = () => {
+  useEffect(() => {
+    const saved = localStorage.getItem('wallpaperUrl');
+    if (saved) {
+      document.documentElement.style.setProperty(
+        '--wallpaper-url', `url('${saved}')`
+      );
+    }
+  }, []);
   return (
     <main>
       <NavBar />
@@ -20,6 +29,8 @@ const App = () => {
       <Text />
       <Finder />
       <Contact />
+      <Home />
+      <Photos />
     </main>
   )
 }
